678148d24cdb9471f458bedc99c803d65b774439
Вадим Козлов <xatiole@gmail.com>
2024-03-25 17:46:22 +0300

    1 Lab

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1_Лабораторная
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ PaintEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace _1_Лабораторная
+{
+    public class PaintEventArgs
+    {
+    }
+}
